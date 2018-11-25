cf1d70a1fd95670e3a1fd9256bc28f1a637ad109
nadaabdulkarem <asghar.nada@gmail.com>
2018-11-25 09:10:04 +0300

    game 1 not complite

--- starter1.js
+++ starter1.js
@@ -1,3 +1,41 @@
 console.log('starter1.js linked');
 
 //code goes here
+$(document).ready(function () {
+    $('.zone').hover(function () {
+            // over
+            $(this).css({
+                        backgroundColor : 'green'
+                    })
+        }, function () {
+            // out
+            $(this).css({
+                        backgroundColor : 'white'
+                    })
+
+            $(this).click(function () { 
+                $(this).unbind('mouseenter').unbind('mouseleave');
+                $(this).css({
+                    backgroundColor : 'green'
+                })
+                
+            });
+        }
+    );
+
+    // $('.zone').on('mouseenter', function(){
+    //     $(this).addClass('green');
+    // });
+
+    // $('.zone').on('click', function(){
+    //     $(this).addClass('green');
+    // });
+
+    // $('.zone').on('mouseleave', function(){
+    //     $(this).css({
+    //         backgroundColor : 'white'
+    //     })
+    // });
+
+
+})
